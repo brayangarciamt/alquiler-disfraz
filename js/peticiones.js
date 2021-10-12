b4a3281b5ef088bcebbe4cc21914010da2fec501
Brayan Garcia <brayan.garcia.mt@correo.usa.edu.co>
2021-10-11 21:37:40 -0500

    Se añaden funciones GET y POST para formulario mensaje

--- js/peticiones.js
+++ js/peticiones.js
@@ -1,3 +1,53 @@
+//Funcion encarga de traer la informacion de todos los mensajes 
+// almacenados en la tabla
 function verListaMensajes(){
-    alert("Funciona");
+        $.ajax (
+                {
+                    url          : 'https://g7c696f5e7eec44-alquiladisfraz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/message/message',
+                    type         : 'GET',
+                    dataType     : 'json',
+                    success      :  function(json){
+                                    $("#respuestaMensaje").empty();
+                                    $("#respuestaMensaje").append("<table>");
+                                    for(i=0 ; i < json.items.length; i++){
+                                        $("#respuestaMensaje").append("<tr><td><a href='https://www.google.com'>"
+                                                                    +json.items[i].id+"</a></td><td>"
+                                                                    +json.items[i].messagetext + "</td></tr>")
+                                    }
+                                    $("#respuestaMensaje").append("</table>");
+                                    console.log(json)
+                                    },
+                    error        :  function(xhr,status){
+                                    console.log(xhr)
+                                    }
+                }
+    );
+}
+
+// Funcion encargada de almacenar el mensaje en la BD - tabla MENSAJE
+function enviarMensaje(){
+    
+    var idMensaje = $("#labelIdMensaje").val();
+    var cuerpoMensaje = $("#labelMensaje").val();
+    var mensaje;
+    mensaje = { id:idMensaje, messagetext:cuerpoMensaje};
+
+    $.ajax (
+                {
+                    url          : 'https://g7c696f5e7eec44-alquiladisfraz.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/message/message',
+                    type         : 'POST',
+                    data         :  mensaje,
+
+                    success      :  function(response){ 
+                                        console.log("respuesta --> ",response);
+                                    },
+                    error       :   function(xhr,status){
+                                        console.log(xhr);
+                                        alert("Algo salio mal ",status);
+                                    },
+                    complete    :   function(xhr, status) {
+                                        alert("Peticion realizada");
+                                    }
+                }
+    );
 }
